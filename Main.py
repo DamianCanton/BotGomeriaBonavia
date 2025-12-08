d65d253d0a9907775fbd9d3b99df78bb5cded210
Damian Canton <damiancanton278@gmail.com>
2025-12-07 22:01:28 -0300

    v2

--- Main.py
+++ Main.py
@@ -16,9 +16,11 @@ from telegram.ext import (
     ContextTypes
 )
 
+TOKEN_TELEGRAM = 7890020254:AAH8Arv39q57dIdWC0zYN4qpWvijSN2LMcE
+
 # --- 1. CONFIGURACIÓN ---
 # Token: Render lo inyecta como variable de entorno
-TOKEN = os.environ.get("7890020254AAH8Arv39q57dIdWC0zYN4qpWvijSN2LMcE")
+TOKEN = os.environ.get("TOKEN_TELEGRAM")
 PORT = int(os.environ.get('PORT', 10000))
 
 # Reglas de Negocio
